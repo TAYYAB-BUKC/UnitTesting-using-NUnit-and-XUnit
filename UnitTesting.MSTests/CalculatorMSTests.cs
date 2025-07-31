99ba33a9d838016053dae6fb0a21c28af72e9fd4
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-01 02:53:50 +0500

    CalculatorMSTests implemented.

---
+++ UnitTesting.MSTests/CalculatorMSTests.cs
@@ -0,0 +1,19 @@
+﻿namespace UnitTesting.MSTests
+{
+	[TestClass]
+	public class CalculatorMSTests
+	{
+		[TestMethod]
+		public void Add_InputTwoInt_GetCorrectAddition()
+		{
+			// Arrange
+			Calculator calculator = new();
+
+			// Act
+			int sum = calculator.Add(1, 2);
+
+			// Assert
+			Assert.AreEqual(3, sum);
+		}
+	}
+}
