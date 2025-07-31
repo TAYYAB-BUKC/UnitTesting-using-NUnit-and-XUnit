826645fd33b4258df1826633ff50f6d829c28039
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-01 03:09:56 +0500

    CalculatorNUnitTests implemented for the first unit test using NUnit.

---
+++ UnitTesting.NUnitTests/CalculatorNUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace UnitTesting.NUnitTests
+{
+	[TestFixture]
+	public class CalculatorNUnitTests
+	{
+		[Test]
+		public void Add_InputTwoInt_GetCorrectAddition()
+		{
+			// Arrange
+			Calculator calculator = new();
+
+			// Act
+			int sum = calculator.Add(1, 2);
+
+			// Assert
+			ClassicAssert.AreEqual(3, sum);
+		}
+	}
+}
