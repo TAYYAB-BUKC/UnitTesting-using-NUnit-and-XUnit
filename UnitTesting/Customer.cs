ed2ecb67d6ea77cca0e49a5be676a03c6daead78
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-01 16:04:40 +0500

    GreetWithFullName implemented in Customer.

---
+++ UnitTesting/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace UnitTesting
+{
+	public class Customer
+	{
+		public string GreetWithFullName(string firstName, string lastName)
+		{
+			return $"Hello, {firstName} {lastName}";
+		}
+	}
+}
