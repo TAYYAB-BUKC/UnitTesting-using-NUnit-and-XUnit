97327b7dada0fbbf0d66fac0b571d00867136270
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-01 16:16:34 +0500

    GreetWithFullName test implementation enhanced by using other string helper methods.

--- UnitTesting.NUnitTests/CustomerNUnitTests.cs
+++ UnitTesting.NUnitTests/CustomerNUnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
 
 namespace UnitTesting.NUnitTests
 {
@@ -15,7 +16,14 @@ namespace UnitTesting.NUnitTests
 			string greetMessage = customer.GreetWithFullName("Ben", "Spark");
 
 			// Assert
+			ClassicAssert.AreEqual(greetMessage, "Hello, Ben Spark");
 			Assert.That(greetMessage, Is.EqualTo("Hello, Ben Spark"));
+
+			Assert.That(greetMessage, Does.StartWith("Hello"));
+			Assert.That(greetMessage, Does.EndWith("Spark"));
+			Assert.That(greetMessage, Does.Contain("Ben"));
+			Assert.That(greetMessage, Does.Match("[A-Z]{1}[a-z]+, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
+			Assert.That(greetMessage, Does.Contain("ben spark").IgnoreCase);
 		}
 	}
 }
