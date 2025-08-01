e053465dc4a72b280da5cc72c458c08d11f99c36
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-01 15:47:46 +0500

    Add overloaded method of type double implemented.

--- UnitTesting/Calculator.cs
+++ UnitTesting/Calculator.cs
@@ -1,11 +1,18 @@
-﻿namespace UnitTesting
+﻿using System;
+
+namespace UnitTesting
 {
     public class Calculator
     {
         public int Add(int a, int b)
         {
             return a + b;
-        }
+		}
+
+		public double Add(double a, double b)
+		{
+			return a + b;
+		}
 
 		public bool IsOddNumber(int a)
 		{
