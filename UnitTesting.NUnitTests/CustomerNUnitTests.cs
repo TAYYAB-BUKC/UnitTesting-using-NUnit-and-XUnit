d03a2c723bbba7cb649a959b768ab3845aaa4d78
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-01 16:09:04 +0500

    GreetWithFullName method test case implemented in CustomerNUnitTests.

---
+++ UnitTesting.NUnitTests/CustomerNUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace UnitTesting.NUnitTests
+{
+	[TestFixture]
+	public class CustomerNUnitTests
+	{
+		[Test]
+		public void GreetWithFullName_InputFirstNameAndLastName_OutputGreetMessage()
+		{
+			// Arrange
+			Customer customer = new Customer();
+
+			// Act
+			string greetMessage = customer.GreetWithFullName("Ben", "Spark");
+
+			// Assert
+			Assert.That(greetMessage, Is.EqualTo("Hello, Ben Spark"));
+		}
+	}
+}
