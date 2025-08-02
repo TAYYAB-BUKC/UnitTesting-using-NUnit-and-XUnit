9a7d82e7354a0b22bfaafe65733c616c5541690f
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-03 03:50:34 +0500

    Test for AGrade implemented in GradingCalculatorNUnitTests.

---
+++ UnitTesting.NUnitTests/GradingCalculatorNUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace UnitTesting.NUnitTests
+{
+	[TestFixture]
+	public class GradingCalculatorNUnitTests
+	{
+		private GradingCalculator gradingCalculator;
+		[SetUp]
+		public void SetUp()
+		{
+			gradingCalculator = new();
+		}
+
+		[Test]
+		public void GetGrade_InputScoreIs95AndAttendanIs90_OutputAGrade()
+		{
+			// Arrange
+			gradingCalculator.Score = 95;
+			gradingCalculator.AttendancePercentage = 90;
+
+			// Act
+			var grade = gradingCalculator.GetGrade();
+
+			// Assert
+			ClassicAssert.AreEqual("A", grade);
+			Assert.That(grade, Is.EqualTo("A"));
+			Assert.That(grade, Is.EqualTo("A").IgnoreCase);
+		}
+	}
+}
