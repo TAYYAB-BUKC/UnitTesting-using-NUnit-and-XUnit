2710e37ca57acae9a50e1072a238a612b5e5bdfd
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-02 23:42:35 +0500

    GetOddNumbersFromRange implemented in Calculator.

--- UnitTesting/Calculator.cs
+++ UnitTesting/Calculator.cs
@@ -1,10 +1,10 @@
-﻿using System;
-
-namespace UnitTesting
+﻿namespace UnitTesting
 {
-    public class Calculator
+	public class Calculator
     {
-        public int Add(int a, int b)
+		public List<int> Numbers { get; set; }
+        
+		public int Add(int a, int b)
         {
             return a + b;
 		}
@@ -17,6 +17,19 @@ namespace UnitTesting
 		public bool IsOddNumber(int a)
 		{
 			return !(a % 2 == 0);
+		}
+
+		public List<int> GetOddNumbersFromRange(int min, int max)
+		{
+			Numbers = new List<int>();
+			for (int i = min; i <= max; i++)
+			{
+				if(!(i % 2 == 0))
+				{
+					Numbers.Add(i);
+				}
+			}
+			return Numbers;
 		}
 	}
 }
