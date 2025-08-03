415483618c826c0ce148291d1d597ac0da992b41
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-03 07:09:38 +0500

    BankAccount class implemented.

---
+++ UnitTesting/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿namespace UnitTesting
+{
+	public class BankAccount
+	{
+		public decimal Balance { get; set; }
+
+		public decimal GetBalance()
+		{
+			return Balance;
+		}
+
+		public bool Deposit(decimal amount)
+		{
+			Balance += amount;
+			return true;
+		}
+
+		public bool WithDraw(decimal amount)
+		{
+			if(Balance <= amount)
+			{
+				Balance -= amount;
+				return true;
+			}
+			return false;
+		}
+	}
+}
