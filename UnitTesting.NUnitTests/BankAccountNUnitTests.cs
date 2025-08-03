c5adbfa6d994588ca1503cd810cb8756076624c0
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-03 07:27:10 +0500

    Test added for Deposit in BankAccountNUnitTests.

---
+++ UnitTesting.NUnitTests/BankAccountNUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using UnitTesting.Services;
+
+namespace UnitTesting.NUnitTests
+{
+	[TestFixture]
+	public class BankAccountNUnitTests
+	{
+		private BankAccount bankAccount;
+		[SetUp]
+		public void ArrangeSetup()
+		{
+			bankAccount = new(new LogBook());
+		}
+
+		[Test]
+		public void Deposit_InputAmountIs100_OutputIsTrue()
+		{
+			// Arrange
+
+			// Act
+			var result = bankAccount.Deposit(100);
+
+			// Assert
+			Assert.That(result, Is.True);
+			Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
+		}
+	}
+}
