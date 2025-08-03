cfb1fe2a86d7707293ca334548f62a71f10eda97
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-03 06:50:16 +0500

    GetFibonacciSeries test implemented for the Range of 1.

---
+++ UnitTesting.NUnitTests/FibonacciNUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace UnitTesting.NUnitTests
+{
+	[TestFixture]
+	public class FibonacciNUnitTests
+	{
+		private Fibonacci? fibonacci;
+		[SetUp]
+		public void SetUp()
+		{
+			// Arrange
+			fibonacci = new Fibonacci();
+		}
+
+		[Test]
+		public void GetFibonacciSeries_InputRangeIs1_OutputFibonacciSeriesTillRange1()
+		{
+			// Arrange
+			fibonacci!.Range = 1;
+
+			// Act
+			var series = fibonacci.GetFibonacciSeries();
+
+			// Assert
+			Assert.That(series, Is.Not.Empty);
+			Assert.That(series, Is.Ordered);
+			Assert.That(series, Is.All.Member(0));
+		}
+	}
+}
