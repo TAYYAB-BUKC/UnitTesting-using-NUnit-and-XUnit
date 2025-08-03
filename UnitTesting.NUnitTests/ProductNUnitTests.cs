e33da26a3e6faccb4015c59b3690c11745262441
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-03 08:09:35 +0500

    GetProductPrice test implemented in ProductNUnitTests.

---
+++ UnitTesting.NUnitTests/ProductNUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace UnitTesting.NUnitTests
+{
+	[TestFixture]
+	public class ProductNUnitTests
+	{
+		private Product product;
+
+		[SetUp]
+		public void ArrangeSetup()
+		{
+			// Arrange
+			product = new Product() { Id = 1, Name = "Product 1", Price = 50 };
+		}
+
+		[Test]
+		public void GetProductPrice_InputPlatinumCustomer_OutputPriceWith80PercentDiscount()
+		{
+			// Arrange
+
+			// Act
+			var result = product.GetPrice(new Customer() { OrderTotal = 110 });
+
+			// Assert
+			Assert.That(result, Is.EqualTo(40));
+
+
+
+		}
+	}
+}
