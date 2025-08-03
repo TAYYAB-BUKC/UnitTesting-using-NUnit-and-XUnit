b4548daeedb8799f7db7b22ad974364f4e441f24
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-03 07:44:59 +0500

    Product class implemented.

---
+++ UnitTesting/PRoduct.cs
@@ -0,0 +1,18 @@
+﻿namespace UnitTesting
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public double Price { get; set; }
+
+		public double GetPrice(Customer customer)
+		{
+			if(customer.GetCustomerDetails() == default(PlatinumCustomerType))
+			{
+				return Price * 0.8;
+			}
+			return Price;
+		}
+	}
+}
