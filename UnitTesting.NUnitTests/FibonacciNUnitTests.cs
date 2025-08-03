956cb53fa7d984bc897ef0a0755fb89e1190cd27
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-03 07:00:15 +0500

    GetFibonacciSeries test implemented for the Range of 6.

--- UnitTesting.NUnitTests/FibonacciNUnitTests.cs
+++ UnitTesting.NUnitTests/FibonacciNUnitTests.cs
@@ -27,5 +27,21 @@ namespace UnitTesting.NUnitTests
 			Assert.That(series, Is.Ordered);
 			Assert.That(series, Has.Member(0));
 		}
+
+		[Test]
+		public void GetFibonacciSeries_InputRangeIs6_OutputFibonacciSeriesTillRange6()
+		{
+			// Arrange
+			fibonacci!.Range = 6;
+			var expectedResult = new List<int> { 0, 1, 1, 2, 3, 5 };
+			// Act
+			var series = fibonacci.GetFibonacciSeries();
+
+			// Assert
+			Assert.That(series, Does.Contain(3));
+			Assert.That(series.Count, Is.EqualTo(6));
+			Assert.That(series, Has.No.Member(4));
+			Assert.That(series, Is.EquivalentTo(expectedResult));
+		}
 	}
 }
