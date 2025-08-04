0a2f99b71dfed92baa6a3bcd520464d35768e0bc
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-04 09:56:56 +0500

    ProductXUnitTests implemented and succesfully converted from NUnit to XUnit.

---
+++ UnitTesting.XUnitTests/ProductXUnitTests.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using UnitTesting.Services.Interfaces;
+using Xunit;
+
+namespace UnitTesting.XUnitTests
+{
+	public class ProductXUnitTests
+	{
+		private Product product;
+
+		public ProductXUnitTests()
+		{
+			// Arrange
+			product = new Product() { Id = 1, Name = "Product 1", Price = 50 };
+		}
+
+		[Fact]
+		public void GetProductPrice_InputPlatinumCustomer_OutputPriceWith80PercentDiscount()
+		{
+			// Arrange
+
+			// Act
+			var result = product.GetPrice(new Customer() { OrderTotal = 110 });
+
+			// Assert
+			Assert.Equal(40, result);
+		}
+
+		[Fact]
+		public void GetProductPriceWithMOQAbuse_InputPlatinumCustomer_OutputPriceWith80PercentDiscount()
+		{
+			// Arrange
+			var moq = new Mock<ICustomer>();
+			moq.Setup(m => m.OrderTotal).Returns(110);
+
+			// Act
+			var result = product.GetPrice(moq.Object);
+
+			// Assert
+			Assert.Equal(40, result);
+		}
+	}
+}
