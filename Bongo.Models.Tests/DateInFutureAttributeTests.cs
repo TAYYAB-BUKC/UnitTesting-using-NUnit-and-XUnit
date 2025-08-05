5d47e6b14f54a59895f92f695fcece7c91113da2
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-05 07:48:34 +0500

    DateInFutureAttribute test implemented in DateInFutureAttributeTests.

---
+++ Bongo.Models.Tests/DateInFutureAttributeTests.cs
@@ -0,0 +1,22 @@
+﻿using Bongo.Models.ModelValidations;
+using NUnit.Framework;
+
+namespace Bongo.Models.Tests
+{
+	[TestFixture]
+	public class DateInFutureAttributeTests
+	{
+		[Test]
+		public void DateValidator_InputIsFutureDate_OutputIsValidDate()
+		{
+			// Arrange
+			DateInFutureAttribute dateInFutureAttribute = new DateInFutureAttribute();
+
+			// Act
+			var isValidDate = dateInFutureAttribute.IsValid(DateTime.Now.AddDays(5));
+
+			// Assert
+			Assert.That(isValidDate, Is.True);
+		}
+	}
+}
