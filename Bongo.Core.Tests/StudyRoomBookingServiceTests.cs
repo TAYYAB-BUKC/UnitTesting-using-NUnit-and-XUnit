892a9f318e09d507083cbe57e5b61c01882e0db5
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-05 09:47:51 +0500

    Arrange phase implemented in StudyRoomBookingServiceTests.

---
+++ Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Bongo.Core.Services;
+using Bongo.DataAccess.Repository.IRepository;
+using Moq;
+using NUnit.Framework;
+
+namespace Bongo.Core.Tests
+{
+	[TestFixture]
+	public class StudyRoomBookingServiceTests
+	{
+		private Mock<IStudyRoomBookingRepository> _studyRoomBookingRepository;
+		private Mock<IStudyRoomRepository> _studyRoomRepository;
+		private StudyRoomBookingService studyRoomBookingService;
+
+		[SetUp]
+		public void ArrangePhase()
+		{
+			_studyRoomBookingRepository = new Mock<IStudyRoomBookingRepository>();
+			_studyRoomRepository = new Mock<IStudyRoomRepository>();
+			studyRoomBookingService = new StudyRoomBookingService(
+				_studyRoomBookingRepository.Object,
+				_studyRoomRepository.Object
+			);
+		}
+	}
+}
